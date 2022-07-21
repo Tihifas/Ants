7e8b39ef71edd99afa09153662b1127b4a979757
Torstein Bratberg <tdbratberg@gmail.com>
2022-07-21 22:56:26 +0200

    Installed UniTask so i can await and use gametime at the same time. Wrestling with wierd rotation after joint

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 
 public class Ant : MonoBehaviour
 {
@@ -12,6 +15,10 @@ public class Ant : MonoBehaviour
     void Start()
     {
         Debug.Log("Start");
+
+        Vector2 destination = new Vector2(1, -1);
+
+        GoGetObjAndMoveTo(destination);
     }
 
     void Update()
@@ -40,7 +47,45 @@ public class Ant : MonoBehaviour
         rb.velocity = velocity;
     }
 
-    public IEnumerator MoveTo(Vector2 target, float? speed = null)
+    //TODO keep?
+    //public IEnumerator MoveTo(Vector2 target, float? speed = null)
+    //{
+    //    if (speed == null) speed = this.speed;
+
+    //    Vector2 vecToTarget = (Vector3)target - this.transform.position;
+
+    //    LookAt(target);
+
+    //    StartWalking();
+
+    //    //This assumes it's speed was not changed!
+    //    float distance = vecToTarget.magnitude;
+    //    float secondsToDestination = distance / (float)speed;
+    //    yield return new WaitForSeconds(secondsToDestination);
+
+    //    SetVelocity(Vector2.zero);
+    //}
+
+    //public async Task MoveToAsync(Vector2 target, float? speed = null)
+    //{
+    //    if (speed == null) speed = this.speed;
+
+    //    Vector2 vecToTarget = (Vector3)target - this.transform.position;
+
+    //    LookAt(target);
+
+    //    StartWalking();
+
+    //    //This assumes it's speed was not changed!
+    //    float distance = vecToTarget.magnitude;
+    //    float secondsToDestination = distance / (float)speed;
+    //    //yield return new WaitForSeconds(secondsToDestination);
+    //    await Task.Delay(TimeSpan.FromSeconds(secondsToDestination));
+
+    //    SetVelocity(Vector2.zero);
+    //}
+
+    public async Task MoveTo3(Vector2 target, float? speed = null)
     {
         if (speed == null) speed = this.speed;
 
@@ -53,15 +98,20 @@ public class Ant : MonoBehaviour
         //This assumes it's speed was not changed!
         float distance = vecToTarget.magnitude;
         float secondsToDestination = distance / (float)speed;
-        yield return new WaitForSeconds(secondsToDestination);
+        //yield return new WaitForSeconds(secondsToDestination);
+        //https://openupm.com/packages/com.cysharp.unitask/
+        //TODO undo!!
+        await UniTask.Delay(TimeSpan.FromSeconds(secondsToDestination), ignoreTimeScale: false);
+        //await UniTask.Delay(TimeSpan.FromSeconds(secondsToDestination*0.8), ignoreTimeScale: false);
 
         SetVelocity(Vector2.zero);
     }
 
-    public IEnumerator MoveToCurrentPosOfObj(GameObject targetObj)
-    {
-        yield return MoveTo(targetObj.transform.position);
-    }
+
+    //public IEnumerator MoveToCurrentPosOfObj(GameObject targetObj)
+    //{
+    //    yield return MoveTo(targetObj.transform.position);
+    //}
 
     //USE THIS? https://answers.unity.com/questions/1244393/best-way-to-move-a-rigidbody2d-from-point-a-to-poi.html
     //TODO move to vector helper
@@ -85,7 +135,9 @@ public class Ant : MonoBehaviour
 
     private void LookAt(Vector2 target)
     {
+        Debug.Log("LookAt target : " + target);
         Vector3 direction = (Vector3)target - this.transform.position;
+        Debug.Log("LookAt direction : " + direction);
         this.transform.up = direction;
     }
 
@@ -100,7 +152,7 @@ public class Ant : MonoBehaviour
         GameObject targetGo = targetCollider.gameObject;
         var pickUpAbleComponent = targetGo.GetComponent<IPickUpAble>();
         bool targetIsPickUppAble = (pickUpAbleComponent != null && pickUpAbleComponent.IsPickUpAble);
-        if (targetIsPickUppAble)
+        if (targetIsPickUppAble && !Carrying)
         {
             pickUpAbleComponent.IsPickUpAble = false;
             PickUpObject(targetGo);
@@ -115,14 +167,25 @@ public class Ant : MonoBehaviour
         Vector2 carryPoint = ((Vector2)this.transform.position) + Forward() * 0.4f;
         target.transform.position = carryPoint;
 
-        FixedJoint2D joint = (FixedJoint2D)this.gameObject.AddComponent(typeof(FixedJoint2D));
-        joint.connectedBody = targetRb;
+        Quaternion ar = this.transform.rotation;
+        target.transform.rotation = this.transform.rotation;
+        //target.transform.rotation = new Quaternion(ar.x, ar.y, ar.z, ar.w);
+
+        //FixedJoint2D joint = (FixedJoint2D)this.gameObject.AddComponent(typeof(FixedJoint2D));
+        //joint.connectedBody = targetRb;
     }
 
-    //private void FetchAPickupAbleAndGoTo(Vector2 destination)
-    //{
-    //    PickUpAble pickUpAble = FindObjectsOfType<PickUpAble>()[0];
-    //    StartCoroutine(MoveTo(pickUpAble.transform.position));
-    //}
+    private async Task GoGetObjAndMoveTo(Vector2 destination)
+    {
+        GameObject pickUpAble = FindObjectsOfType<PickUpAble>()[0].gameObject;
+        //StartCoroutine(MoveTo(pickUpAble.transform.position));
+        await MoveTo3(pickUpAble.transform.position);
+
+        FixedJoint2D joint = (FixedJoint2D)this.gameObject.AddComponent(typeof(FixedJoint2D));
+        joint.connectedBody = pickUpAble.GetComponent<Rigidbody2D>();
+
+        //await MoveTo3(destination);
+        LookAt(destination);
+    }
 
 }
